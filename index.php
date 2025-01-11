ba3b7375decf6d76ccacd44e36faa692a5335b1b
An0sun <164004575+An0sun@users.noreply.github.com>
2025-01-11 23:06:18 +0100

    Update index.php V3

--- index.php
+++ index.php
@@ -1,11 +1,16 @@
 <?php
 session_start();
 
+// Activer l'affichage des erreurs pour le débogage
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
 // Inclusion des classes nécessaires
 require_once "connexion.php";
 require_once "site.php";
-require_once "composants/menu/controleur_menu.php";
-require_once "composants/footer/controleur_footer.php";
+require_once "vue_generique.php";
+require_once "module_generique.php";
 
 // Initialisation de la connexion
 Connexion::init_connexion();
@@ -17,18 +22,6 @@ $site->exec_module();
 // Préparation des variables pour le template
 $titre = $site->get_module()->get_title();
 $module_html = $site->get_module()->get_affichage();
-
-// Génération du menu
-$menuControleur = new ControleurMenu();
-ob_start();
-$menuControleur->afficher_menu();
-$menu_html = ob_get_clean();
-
-// Génération du footer
-$footerControleur = new ControleurFooter();
-ob_start();
-$footerControleur->afficher_footer();
-$footer_html = ob_get_clean();
 
 // Inclusion du template
 include_once "template.php";
